af3e3555486f4a05a79c12305172892385253894
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2024-08-12 18:28:23 -0300

    Update script.js
    Adicionado as funcionalidades para alterar cor do texto, fundo do texto e download do arquivo.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let output = document.querySelector('#output')
+// let output = document.querySelector('#output')
 let buttons = document.querySelectorAll('.tool--btn')
+
 for (let btn of buttons) {
     btn.addEventListener('click', () => {
         let cmd = btn.dataset['command']
@@ -10,4 +11,50 @@ for (let btn of buttons) {
             document.execCommand(cmd, false, null)
         }
     })
+}
+
+function formatDoc(cmd, value=null) {
+	if(value) {
+		document.execCommand(cmd, false, value);
+	} else {
+		document.execCommand(cmd);
+	}
+}
+
+function addLink() {
+	const url = prompt('Insert url');
+	formatDoc('createLink', url);
+}
+
+const content = document.querySelector('#content');
+
+content.addEventListener('mouseenter', function () {
+	const a = content.querySelectorAll('a');
+	a.forEach(item=> {
+		item.addEventListener('mouseenter', function () {
+			content.setAttribute('contenteditable', false);
+			item.target = '_blank';
+		})
+		item.addEventListener('mouseleave', function () {
+			content.setAttribute('contenteditable', true);
+		})
+	})
+})
+
+const filename = document.getElementById('filename');
+
+function fileHandle(value) {
+	if(value === 'new') {
+		content.innerHTML = '';
+		filename.value = 'sem titulo';
+	} else if(value === 'txt') {
+		const blob = new Blob([content.innerText])
+		const url = URL.createObjectURL(blob)
+		const link = document.createElement('a');
+		link.href = url;
+		link.download = `${filename.value}.txt`;
+		link.click();
+	} else if(value === 'pdf') {
+		html2pdf(content).save(filename.value);
+	}
 }
