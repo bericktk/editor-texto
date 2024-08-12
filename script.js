9bd7044ff0ef96c874d3ea0336a7b6020b80c753
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2024-08-12 17:57:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let output = document.querySelector('#output')
+let buttons = document.querySelectorAll('.tool--btn')
+for (let btn of buttons) {
+    btn.addEventListener('click', () => {
+        let cmd = btn.dataset['command']
+        if (cmd === 'createlink') {
+            let url = prompt("Enter the link here: ", "http:\/\/")
+            document.execCommand(cmd, false, url)
+        } else {
+            document.execCommand(cmd, false, null)
+        }
+    })
+}
